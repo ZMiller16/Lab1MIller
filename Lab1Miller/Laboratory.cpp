17bc7ebdcfd07800f6e8ead0f892da8b6bb7f34a
Zach Miller <zmiller16@rockets.utoledo.edu>
2024-04-04 13:03:04 -0400

    first commit

---
+++ Lab1Miller/Laboratory.cpp
@@ -0,0 +1,26 @@
+/*
+ * Program:  <First Laboratory>
+ * Author:   <Zachary Miller>
+ * Date:     <January 25, 2024>
+ *
+ */
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+int main(void) {
+
+	float width;
+	float length;
+	float area;
+	length = 4.5;
+	width = 3.35;
+	area = length * width;
+	cout << "Rectangle area = " << area << "\n'";
+
+	cout << "Who's on first? \n" ;
+
+	return 0;
+}
